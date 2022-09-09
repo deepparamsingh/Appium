fd986f2a22b4b71fb78d275a35f16d13c21c5b31
Paramdeep Singh <deepparamsingh@gmail.com>
2022-09-09 09:38:56 +0530

    08-09-2022 11:00PM

---
+++ src/main/java/Udemy/Appium/HomePage.java
@@ -0,0 +1,44 @@
+package Udemy.Appium;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class HomePage {
+	
+	static AppiumDriver<MobileElement> driver;
+
+	public static void main(String[] args) {
+		
+		try {
+		openApp();
+		}
+		catch (Exception e) {
+			
+		}
+	
+
+	}
+	
+	public static void openApp() throws MalformedURLException
+	{
+		DesiredCapabilities cap= new DesiredCapabilities();
+		cap.setCapability("deviceName", "honor 7A");
+		cap.setCapability("udid", "XUV3Y18823027827");
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "8.0.0");
+		cap.setCapability("appPackage", "io.appium.android.apis");
+		cap.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+		cap.setCapability("autoAcceptAlerts", true);
+		cap.setCapability("autoDismissAlerts", false);
+	
+		URL url= new URL("http://127.0.0.1:4723/wd/hub");
+		driver=new AppiumDriver<MobileElement>(url,cap);
+		
+		System.out.println("Application Started...");
+	}
+
+}
